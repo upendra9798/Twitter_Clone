a0709b6ce203e2dbcd9f33e42c8bc3b4f1d1f16a
upendra9798 <upendrakushwaha022@gmail.com>
2025-08-01 10:26:32 +0530

    feat: Implement sidebar component with user profile and navigation links
    feat: Add post skeleton component for loading states
    
    feat: Create profile header skeleton for user profile loading states
    
    feat: Develop right panel skeleton for loading states
    
    feat: Add SVG component for custom icon rendering
    
    feat: Build login page with form handling and validation
    
    feat: Create signup page with form handling and validation
    
    feat: Implement create post component with image upload functionality
    
    feat: Develop home page with post creation and feed display
    
    feat: Create notification page with loading state and notification display
    
    feat: Implement edit profile modal for user profile updates
    
    feat: Build profile page with user information and post display
    
    feat: Add dummy data for posts and users for development purposes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+import "./App.css";
+import {Route,Routes} from 'react-router-dom'
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import HomePage from "./pages/home/HomePage";
+import NotificationPage from "./pages/notification/Notification";
+import ProfilePage from "./pages/Profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
-
   return (
-    <>
-     <p className='text-5xl text-red-500 bg-blue-300'>Hello there </p>
-    </>
-  )
+   <div className='flex max-w-6xl mx-auto'>
+			{/* Common component, bc it's not wrapped with Routes */}
+			{/* {authUser && <Sidebar />} */}
+			<Sidebar />
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/notifications' element={<NotificationPage />} />
+				<Route path='/profile/:username' element={<ProfilePage />} />
+			</Routes>
+			{/* {authUser && <RightPanel />} */}
+			<RightPanel />
+			{/* <Toaster /> */}
+		</div>
+  );
 }
 
-export default App
+export default App;
