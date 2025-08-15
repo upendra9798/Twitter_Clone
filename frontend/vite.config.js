fe71dc7fee3cafc3b3212256fa74abb90a98efe3
upendra9798 <upendrakushwaha022@gmail.com>
2025-08-15 19:49:53 +0530

    fix: api target fixed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api":{
-      target: "http://localhost:5000",
+      target: "https://twitter-clone-27ho.onrender.com",
       changeOrigin: true,
     },
   }
