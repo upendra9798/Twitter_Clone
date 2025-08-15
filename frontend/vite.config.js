2eb5c8cd6822415781a9404338fe002df55f4826
upendra9798 <upendrakushwaha022@gmail.com>
2025-08-15 22:50:04 +0530

    /api fix

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://twitter-clone-27ho.onrender.com',
+        target: 'https://twitter-clone-27ho.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
         configure: (proxy, _options) => {
