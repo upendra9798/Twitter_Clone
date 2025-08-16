951d6b60dd161c3598a4c5a18fd3a2c9655bd4ff
upendra9798 <upendrakushwaha022@gmail.com>
2025-08-16 15:57:59 +0530

    import fix

--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -10,6 +10,7 @@ import { MdDriveFileRenameOutline } from "react-icons/md";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import toast from "react-hot-toast";
 import axios from "axios";
+import { makeRequest } from "../../../utils/api";
 //Note:- React Query (now renamed TanStack Query) is a powerful data-fetching
 //  and state management library for React applications.
 // React Query helps you fetch, cache, update, and sync data from APIs effortlessly —
